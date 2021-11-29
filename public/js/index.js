cf6a117eec53aa1ba12b8d5df3c98104a1fda49a
VuNgN <vunguyen220901@gmail.com>
2021-11-29 23:01:50 +0700

    update home page

--- public/js/index.js
+++ public/js/index.js
@@ -1,2 +1,29 @@
 "use strict";
-console.log('Hello world');
+const guideEntryElement = document.querySelectorAll('.body-left__guide-entry');
+const clearActiveElement = () => {
+    guideEntryElement.forEach((guide) => {
+        const childOfGuide = guide.children;
+        for (let child in childOfGuide) {
+            if (childOfGuide[child].firstElementChild) {
+                const liElement = childOfGuide[child].firstElementChild;
+                liElement.classList.remove('guide-entry__item-active');
+            }
+        }
+    });
+};
+guideEntryElement.forEach((guide) => {
+    guide.addEventListener('click', (e) => {
+        const target = e.target;
+        if (target.localName === 'li') {
+            clearActiveElement();
+            target.classList.add('guide-entry__item-active');
+        }
+        else if (target.localName === 'span' || target.localName === 'p') {
+            const parentElement = target.parentElement;
+            if (parentElement.localName === 'li') {
+                clearActiveElement();
+                parentElement.classList.add('guide-entry__item-active');
+            }
+        }
+    });
+});
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,30 @@
-console.log('Hello world');
+const guideEntryElement = document.querySelectorAll<HTMLUListElement>('.body-left__guide-entry');
 
+const clearActiveElement = () => {
+  guideEntryElement.forEach((guide: HTMLUListElement) => {
+    const childOfGuide = guide.children as HTMLCollection;
+    for (let child in childOfGuide) {
+      if (childOfGuide[child].firstElementChild) {
+        const liElement = childOfGuide[child].firstElementChild as HTMLLIElement;
+        liElement.classList.remove('guide-entry__item-active');
+      }
+    }
+  });
+}
+
+guideEntryElement.forEach((guide: HTMLUListElement) => {
+  guide.addEventListener('click', (e: Event) => {
+    const target = e.target as HTMLElement;
+    if (target.localName === 'li') {
+      clearActiveElement();
+      target.classList.add('guide-entry__item-active');
+    } else if (target.localName === 'span' || target.localName === 'p') {
+      const parentElement = target.parentElement as HTMLElement;
+      if (parentElement.localName === 'li') {
+        clearActiveElement();
+        parentElement.classList.add('guide-entry__item-active');
+
+      }
+    }
+  });
+});
