055fb39ed13bfc85d97046a08c5631f6e4b5cbbc
VuNgN <vunguyen220901@gmail.com>
2021-11-30 08:54:58 +0700

    update home-page and logic

--- public/js/index.js
+++ public/js/index.js
@@ -1,41 +1,26 @@
 "use strict";
+function clearActiveElement(elements, idElement) {
+    elements.forEach((guide) => {
+        guide.classList.remove(idElement);
+    });
+}
+function onClickElement(elements, idElement) {
+    elements.forEach((guide) => {
+        guide.addEventListener('click', (e) => {
+            const target = e.target;
+            if (target.localName === guide.localName) {
+                clearActiveElement(elements, idElement);
+                target.classList.add(idElement);
+            }
+            else {
+                const parentElement = target.parentElement;
+                clearActiveElement(elements, idElement);
+                parentElement.classList.add(idElement);
+            }
+        });
+    });
+}
 const guideEntryElement = document.querySelectorAll('.guide-entry__item');
-const clearGuideActiveElement = () => {
-    guideEntryElement.forEach((guide) => {
-        guide.classList.remove('guide-entry__item-active');
-    });
-};
-guideEntryElement.forEach((guide) => {
-    guide.addEventListener('click', (e) => {
-        const target = e.target;
-        if (target.localName === 'li') {
-            clearGuideActiveElement();
-            target.classList.add('guide-entry__item-active');
-        }
-        else if (target.localName === 'span' || target.localName === 'p') {
-            const parentElement = target.parentElement;
-            clearGuideActiveElement();
-            parentElement.classList.add('guide-entry__item-active');
-        }
-    });
-});
 const chipsElements = document.querySelectorAll('.body-chips__chip');
-const clearChipActiveElement = () => {
-    chipsElements.forEach((chipElement) => {
-        chipElement.classList.remove('body-chips__chip-active');
-    });
-};
-chipsElements.forEach((chipElement) => {
-    chipElement.addEventListener('click', (e) => {
-        const target = e.target;
-        if (target.localName === 'div') {
-            clearChipActiveElement();
-            target.classList.add('body-chips__chip-active');
-        }
-        else {
-            const parentElement = target.parentElement;
-            clearChipActiveElement();
-            parentElement.classList.add('body-chips__chip-active');
-        }
-    });
-});
+onClickElement(guideEntryElement, "guide-entry__item-active");
+onClickElement(chipsElements, "body-chips__chip-active");
