fb1ab75389a53dd94f5b6937c9d9d6be08fa7313
VuNgN <vunguyen220901@gmail.com>
2021-11-30 08:51:45 +0700

    update home-page and logic

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,36 @@
-const guideEntryElement = document.querySelectorAll<HTMLLIElement>('.guide-entry__item');
-const clearGuideActiveElement = () => {
-  guideEntryElement.forEach((guide: HTMLLIElement) => {
-    guide.classList.remove('guide-entry__item-active')
+function clearActiveElement<T extends {forEach: Function}>(
+  elements: T,
+  idElement: string
+): void {
+  elements.forEach((guide: HTMLLIElement) => {
+    guide.classList.remove(idElement);
   });
 }
-guideEntryElement.forEach((guide: HTMLLIElement) => {
-  guide.addEventListener('click', (e: Event) => {
-    const target = e.target as HTMLElement;
-    if (target.localName === 'li') {
-      clearGuideActiveElement();
-      target.classList.add('guide-entry__item-active');
-    } else if (target.localName === 'span' || target.localName === 'p') {
-      const parentElement = target.parentElement as HTMLElement;
-      clearGuideActiveElement();
-      parentElement.classList.add('guide-entry__item-active');
-    }
+function onClickElement<T extends {forEach: Function}>(
+  elements: T,
+  idElement: string
+): void {
+  elements.forEach((guide: HTMLElement) => {
+    guide.addEventListener('click', (e: Event) => {
+      const target = e.target as HTMLElement;
+      if (target.localName === guide.localName) {
+        clearActiveElement(elements, idElement);
+        target.classList.add(idElement);
+      } else {
+        const parentElement = target.parentElement as HTMLElement;
+        clearActiveElement(elements, idElement);
+        parentElement.classList.add(idElement);
+      }
+    });
   });
-});
-
-const chipsElements = document.querySelectorAll<HTMLDivElement>('.body-chips__chip');
-const clearChipActiveElement = () => {
-  chipsElements.forEach((chipElement: HTMLDivElement) => {
-    chipElement.classList.remove('body-chips__chip-active');
-  })
 }
-chipsElements.forEach((chipElement: HTMLDivElement) => {
-  chipElement.addEventListener('click', (e: Event) => {
-    const target = e.target as HTMLElement;
-    if (target.localName === 'div') {
-      clearChipActiveElement();
-      target.classList.add('body-chips__chip-active');
-    } else {
-      const parentElement = target.parentElement as HTMLElement;
-      clearChipActiveElement();
-      parentElement.classList.add('body-chips__chip-active');
-    }
-  });
-})
 
+const guideEntryElement = document.querySelectorAll<HTMLLIElement>(
+  '.guide-entry__item'
+);
+const chipsElements = document.querySelectorAll<HTMLDivElement>(
+  '.body-chips__chip'
+);
+
+onClickElement(guideEntryElement, "guide-entry__item-active");
+onClickElement(chipsElements, "body-chips__chip-active");
